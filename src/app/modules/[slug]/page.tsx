06a149a2f71693bcd277dc1db7f261f4ca4a67f1
vincent-guizot <vincentguizot128@gmail.com>
2026-06-12 01:00:53 +0700

    App page Modules DONE

--- src/app/modules/[slug]/page.tsx
+++ src/app/modules/[slug]/page.tsx
@@ -2,7 +2,13 @@ import { notFound } from "next/navigation";
 
 import ModuleContent from "./ModuleContent";
 
-import { MODULES } from "@/features/modules/constants/modules.constants";
+import GithubService from "@/services/github.service";
+
+import { mapModule } from "@/features/modules/mappers/module.mapper";
+
+import { mapLesson } from "@/features/lessons/mappers/lesson.mapper";
+import { mapAssignment } from "@/features/assignments/mappers/assignment.mapper";
+import { mapResource } from "@/features/resources/mappers/resource.mapper";
 
 interface ModuleDetailPageProps {
   params: Promise<{
@@ -10,16 +16,94 @@ interface ModuleDetailPageProps {
   }>;
 }
 
+function slugify(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-");
+}
+
 export default async function ModuleDetailPage({
   params,
 }: ModuleDetailPageProps) {
   const { slug } = await params;
 
-  const module = MODULES.find((item) => item.slug === slug);
+  const programs = await GithubService.getPrograms();
 
-  if (!module) {
+  let selectedModule = null;
+
+  for (const program of programs) {
+    const modules = await GithubService.getModules(program.name);
+
+    const found = modules.find(
+      (module: any) =>
+        `${slugify(program.name)}-${slugify(module.name)}` === slug,
+    );
+
+    if (!found) {
+      continue;
+    }
+
+    const moduleData = mapModule(found, slugify(program.name), program.name);
+
+    try {
+      const lessons = await GithubService.getLessons(program.name, found.name);
+
+      moduleData.lessons = Array.isArray(lessons)
+        ? lessons
+            .filter((item: any) => item.type === "dir")
+            .map((item: any) => mapLesson(item, program.name, found.name))
+        : [];
+
+      console.log("LESSONS");
+      console.log(lessons);
+    } catch {
+      moduleData.lessons = [];
+    }
+
+    try {
+      const assignments = await GithubService.getAssignments(
+        program.name,
+        found.name,
+      );
+
+      moduleData.assignments = Array.isArray(assignments)
+        ? assignments
+            .filter((item: any) => item.type === "dir")
+            .map((item: any) => mapAssignment(item, program.name, found.name))
+        : [];
+
+      console.log("ASSIGNMENTS");
+      console.log(assignments);
+    } catch {
+      moduleData.assignments = [];
+    }
+
+    try {
+      const resources = await GithubService.getResources(
+        program.name,
+        found.name,
+      );
+
+      moduleData.resources = Array.isArray(resources)
+        ? resources
+            .filter((item: any) => item.type !== "dir")
+            .map((item: any) => mapResource(item))
+        : [];
+      console.log("RESOURCES");
+      console.log(resources);
+    } catch {
+      moduleData.resources = [];
+    }
+
+    selectedModule = moduleData;
+
+    break;
+  }
+
+  if (!selectedModule) {
     notFound();
   }
 
-  return <ModuleContent module={module} />;
+  return <ModuleContent module={selectedModule} />;
 }
